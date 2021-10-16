684a0711569f9b720f4ae499bf044d5f0d890444
coinerwh <coinerwh@gmail.com>
2021-10-16 14:12:44 -0700

    refactored frontend to use JS

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,8 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Job Tracker App
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
